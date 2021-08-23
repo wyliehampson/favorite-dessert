5f7a8470bb96be48430b3dbc9d4c220fa5f81e00
juliaparish <jparish@bren.ucsb.edu>
2021-08-23 16:06:55 -0700

    updated iconic desserts

---
+++ desserts_match_myinitials.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(here)
+library(janitor)
+
+dessert_iconic <- read_csv(here("data","iconic_desserts.csv")) %>% 
+  clean_names()
+
+our_desserts <- read_csv(here("favorite_desserts.csv")) %>% 
+  clean_names() %>% 
+  mutate(across(tolower(1:3))
+
+str_detect(string = dessert_iconic$dessert, pattern = our_desserts$Favorite_dessert)                           
+
+
+for (i in 1:length(our_desserts)) {
+  if(our_desserts$first_name[i] == "Julia") {
+    if (our_desserts[i] %in% dessert_iconic$dessert) {
+      print("iconic")
+    } else {
+      print("unique")
+    }
+  }
+}
