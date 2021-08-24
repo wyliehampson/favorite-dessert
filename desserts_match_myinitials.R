abc0c6d2a492ca8f7ae85caec4b68823a62ab44f
wyliehampson <whampson@bren.ucsb.edu>
2021-08-24 23:21:43 +0000

    Uploading using the server practice

--- desserts_match_myinitials.R
+++ desserts_match_myinitials.R
@@ -57,7 +57,7 @@ for (i in seq_along(our_desserts$Favorite_dessert)) {
   if (our_desserts$Favorite_dessert[i] %in% dessert_df$dessert) {
     print(paste(our_desserts$first_name[i], our_desserts$last_name[i], ": Match!"))
   } else {
-    print(paste(our_desserts$first_name[i], our_desserts$last_name[i], ": No Match."))
+    print(paste(our_desserts$first_name[i], our_desserts$last_name[i], ": Not a Match."))
   }
 }
 
